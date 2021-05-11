28099327cd45cf54e0060be26b1459489a2c7213
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-11 10:17:14 -0700

    add Volume Method
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>
    co-authored-by: Michael Burton <micbur1@gmail.com>

--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -2,6 +2,20 @@ namespace EpicodusShipping.Models
 {
   public class Parcel
   {
+    public double[] Dimensions { get; set; }  // decimal
+    public double Weight { get; set; }
 
+    public Parcel(double weight, double x, double y, double z)
+    {
+      Weight = weight;
+      Dimensions = new double[3];
+      Dimensions[0] = x;
+      Dimensions[1] = y;
+      Dimensions[2] = z;
+    }
+    public double Volume()
+    {
+      return Dimensions[0] * Dimensions[1] * Dimensions[2];
+    }
   }
 }

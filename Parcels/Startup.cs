dd384c9402514ffa94deb9701210c46c92d25609
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-11 10:17:50 -0700

    typo

--- Parcels/Startup.cs
+++ Parcels/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.StaticFiles;
 
-namespace Template
+namespace EpicodusShipping
 {
   public class Startup
   {

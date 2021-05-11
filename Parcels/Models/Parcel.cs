439c21d02880dc11e43b213da1bee2eefb5a7782
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-11 13:52:20 -0700

    add Size constructor
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>
    Co-Authored-by: Michael Burton <micbur1@gmail.com>

--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -1,13 +1,34 @@
+using System.Collections.Generic;
+
 namespace EpicodusShipping.Models
 {
-  public class Parcel
+  public class Size
   {
     public double[] Dimensions { get; set; }
+    public Size(double x, double y, double z)
+    {
+      Dimensions = new double[3];
+      Dimensions[0] = x;
+      Dimensions[1] = y;
+      Dimensions[2] = z;
+    }
+  }
+  public class Parcel : Size
+  {
+    public static Dictionary<string, Size> StandardSizes = new()
+    {
+      { "Small", new Size(8.63, 5.5, 1.75) },
+      { "Medium", new Size(11.25, 6, 8.75) },
+      { "Large", new Size(12.25, 12.25, 6) },
+    };
+
     public double Weight { get; set; }
 
     public Parcel(double w, double x, double y, double z)
+    : base(x, y, z)
     {
       Weight = w;
+
       Dimensions = new double[3];
       Dimensions[0] = x;
       Dimensions[1] = y;
@@ -19,3 +40,15 @@ namespace EpicodusShipping.Models
     }
   }
 }
+
+// class Size {
+//   constructor(whatever) {
+//     this.whatever = whatever
+//   }
+// }
+// class Parcel extends Size {
+//   constructor(whatever, whateverElse) {
+//     super(whatever)
+//     this.whateverElse = whateverElse
+//   }
+// }
